20c1cb23cf1e393908738050052526dfbe1857ff
luisrivas123 <luis.rivas@correounivalle.edu.co>
2025-01-16 17:27:36 +0100

    config firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,15 @@
+// config/firebaseConfig.js
+const admin = require('firebase-admin')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const serviceAccount = require('./credentialsFirebase.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://palacio-de-ferias.firebaseio.com'
+})
+
+const db = admin.firestore()
+
+module.exports = { admin, db }
